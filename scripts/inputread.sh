881c4b6bc6eb62c0037bdadf054a349c418cbb31
naveensaiv <vnaveensai@gmail.com>
2020-01-14 18:45:40 +0530

    commiting the changes for inputread file for first time

---
+++ scripts/inputread.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+#read -p 'enter your name' uname
+#read -sp 'enter your password' passwd
+echo "enter ur name:"
+read uname
+echo "enter ur passwd"
+read passwd
+echo "ur name: $uname and passwd: $passwd "
